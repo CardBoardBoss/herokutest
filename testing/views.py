fb59127b52ab31722d43e9d010cf853b29e9f8e3
CardBoardBoss <somecoolsong@gmail.com>
2022-07-03 12:17:48 -0400

    Update views.py

--- testing/views.py
+++ testing/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
  # Create your views here.
 
 def index(request):
-    return render(request, "honey/index.html", {
+    return render(request, "testing/index.html", {
     })
